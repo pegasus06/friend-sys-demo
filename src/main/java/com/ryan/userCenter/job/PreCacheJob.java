b120de9a9ddc7357cd17eefbb1705067f82b475f
zhourui <1010656935@qq.com>
2025-09-18 13:58:12 +0800

    添加定时任务写缓存

---
+++ src/main/java/com/ryan/userCenter/job/PreCacheJob.java
@@ -0,0 +1,43 @@
+package com.ryan.userCenter.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ryan.userCenter.domain.User;
+import com.ryan.userCenter.service.UserService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class PreCacheJob {
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private final List<Long> mainUserList = List.of(1L);
+
+
+    @Scheduled(cron = "0 */5 * * * *")
+    public void doRecommendCache() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        Page<User> page = userService.page(new Page<>(1, 20), wrapper);
+        String format = String.format("ryan:user:recommend:%s", mainUserList);
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        try {
+            valueOperations.set(format, page, 30000, TimeUnit.MILLISECONDS);
+        } catch (Exception e) {
+            log.info("redis write error");
+        }
+
+    }
+}
